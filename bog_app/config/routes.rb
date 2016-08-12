d9cc9891e059f10d27535822637624f144259047
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-08-12 15:48:17 -0700

    make delete button to work

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/creatures/:id", to: "creatures#show", as: "creature"
   get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
   patch "/creatures/:id", to: "creatures#update"
+  delete "/creatures/:id", to: "creatures#destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
