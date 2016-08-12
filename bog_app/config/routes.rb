760c385acac5f50a7c5960b96fd8350bdaa99914
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-08-12 15:43:13 -0700

    make edit,update buttons to work

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "/creatures/new", to: "creatures#new", as: 'new_creature'
   post "/creatures", to: "creatures#create"
   get "/creatures/:id", to: "creatures#show", as: "creature"
+  get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+  patch "/creatures/:id", to: "creatures#update"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
